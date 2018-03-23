38a7c59b3a1139dcb6ca29a5b25cb62ba8050576
Sagar Patil <sagar010797@gmail.com>
2018-03-24 01:29:02 +0530

    dynamic stock graph added

--- stock_graph.py
+++ stock_graph.py
@@ -1,24 +1,40 @@
+import pandas_datareader.data as web
+import datetime
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+from dash.dependencies import Input, Output
 
 app = dash.Dash()
 
 app.layout = html.Div(children=[
-    html.H1(children='Simple plot using Dash'),
-    dcc.Graph(
-        id='example',
+    html.Div(children='''
+        Symbol to graph:
+    '''),
+    dcc.Input(id='input', value='', type='text'),
+    html.Div(id='output-graph'),
+])
+
+@app.callback(
+    Output(component_id='output-graph', component_property='children'),
+    [Input(component_id='input', component_property='value')]
+)
+def update_value(input_data):
+    start = datetime.datetime(2015, 1, 1)
+    end = datetime.datetime.now()
+    df = web.DataReader(input_data, 'iex', start, end)
+
+    return dcc.Graph(
+        id='example-graph',
         figure={
             'data': [
-                {'x': [1, 2, 3, 4, 5, 6], 'y': [9, 6, 4, 2, 1, 0], 'type': 'line', 'name': 'Bikes'},
-                {'x': [1, 2, 3, 4, 5, 6], 'y': [8, 7, 2, 9, 5, 3], 'type': 'bar', 'name': 'Cars'},
+                {'x': df.index, 'y': df.Close, 'type': 'line', 'name': input_data},
             ],
             'layout': {
-                'title': 'Basic Dash Example'
+                'title': input_data
             }
         }
     )
-])
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run_server(debug=True)
